22df98ed05e893beb1b94483135fba5e4d57731d
Cris <cr.huinca@duocuc.cl>
2024-06-14 12:44:58 -0400

    faltan solo validaciones basicas

--- funciones.py
+++ funciones.py
@@ -1,5 +1,6 @@
 import os, time, msvcrt
 trabajadores = []
+cargos = ("CEO", "DESARROLLADOR", "ANALISTA")
 
 def opcion_1():
     os.system("cls")
@@ -12,7 +13,7 @@ def opcion_1():
     desc_salud = int(7/100 * sueldo_bruto)
     desc_afp = int(12/100 * sueldo_bruto)
     sueldo_liquido = round(sueldo_bruto-desc_afp-desc_salud)
-    trabajador = [nombre_apellido , cargo , sueldo_bruto ,desc_afp , desc_salud , sueldo_liquido]
+    trabajador = [nombre_apellido , cargos[cargo - 1] , sueldo_bruto ,desc_afp , desc_salud , sueldo_liquido] #Se mete en la tupla "cargos" y resta 1
     trabajadores.append(trabajador)
     print("TRABAJADOR REGISTRADO CON EXITO!") 
 
@@ -31,18 +32,18 @@ def opcion_3():
     if len(trabajadores) == 0:
         print("No existen trabajadores, elija la opción 1")
     else:
-        opc2 = int(input("¿Que cargo desea imprimir (1.CEO 2.DESARROLLADOR 3.ANALISTA 4.TODOS)"))
+        opc2 = int(input("¿Que cargo desea imprimir (1.CEO 2.DESARROLLADOR 3.ANALISTA 4.TODOS): "))
         if opc2== 4:
             with open("todos_trabajadores.txt", "w", newline="\n" )as archivo:
                 for t in trabajadores:
-                    texto = f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}"
+                    texto = f"NOMBRE : {t[0]}\n CARGO : {t[1]}\n SUELDO BRUTO : {t[2]}\n DES. SALUD : {t[3]}\n DESC. AFP : {t[4]}\n SUELDO LIQUIDO : {t[5]}\n\n"
                     archivo.write(texto)
                 
         else:
             with open("trabajadores_por_cargo.txt","w" , newline= "") as archivo:
                 for t in trabajadores:
-                    if opc2 == t[1]:
-                        texto = f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}"
+                    if opc2 == cargos[opc2 - 1]:
+                        texto = f"NOMBRE : {t[0]}\n CARGO : {t[1]}\n SUELDO BRUTO : {t[2]}\n DES. SALUD : {t[3]}\n DESC. AFP : {t[4]}\n SUELDO LIQUIDO : {t[5]}\n\n"
                         archivo.write(texto)
         print("ARCHIVO CREADO CON EXITO")
 
