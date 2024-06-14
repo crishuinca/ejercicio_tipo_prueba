21323d51efbdc34cc4498af8d86f5cce639e3705
Cris <cr.huinca@duocuc.cl>
2024-06-14 12:14:07 -0400

    Opc 3 finish

--- ej_prueba.py
+++ ej_prueba.py
@@ -54,7 +54,24 @@ while True:
                     print(f"{t[0]}\t{t[1]}\t\t{t[2]}\t\t{t[3]}\t\t{t[4]}\t\t{t[5]}")
 
     elif opc== 3:
-        pass
+        if len(trabajadores) == 0:
+            print("No existen trabajadores, elija la opción 1")
+        else:
+            opc2 = int(input("¿Que cargo desea imprimir (1.CEO 2.DESARROLLADOR 3.ANALISTA 4.TODOS)"))
+            if opc2== 4:
+                with open("todos_trabajadores.txt", "w", newline="\n" )as archivo:
+                    for t in trabajadores:
+                        texto = f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}"
+                        archivo.write(texto)
+                    
+            else:
+                with open("trabajadores_por_cargo.txt","w" , newline= "") as archivo:
+                    for t in trabajadores:
+                        if opc2 == t[1]:
+                            texto = f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}"
+                            archivo.write(texto)
+            print("ARCHIVO CREADO CON EXITO")
+            
     elif opc == 4:
         print("Hasta pronto!!")
         time.sleep(1)
